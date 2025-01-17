49e4087d296b6a91972bf18f3fac672d23ba304d
SillyCatto <raiyanmuhtasim@gmail.com>
2025-01-17 19:19:09 +0600

    setup multiple clients

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const path = require("node:path");
-
+const { Server } = require("socket.io");
 const app = express();
 const PORT = 3000;
 
 const server = app.listen(PORT, () => {
   console.log(`🗨️ server running on port:${PORT}`);
+});
+
+let connectedSockets = new Set();
+
+const io = new Server(server);
+io.on("connection", (socket) => {
+  console.log("Socket connected: ", socket.id);
+  connectedSockets.add(socket.id);
+  io.emit("clients-total", connectedSockets.size);
 });
